0c144c1d51c403ef2e9c5ede6966a655c2d7381c
nickboldt <nboldt@redhat.com>
2021-01-29 14:59:46 -0500

    don't pull/push if branch already exists, and we're not using --force
    Change-Id: I7ae9e10b430af99df81cf6618194c317e4e9b099
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- .github/bin/make-branch.sh
+++ .github/bin/make-branch.sh
@@ -30,15 +30,14 @@ fi
 # create new branch off ${BASEBRANCH} (recreate only if --force'd)
 if [[ "${BASEBRANCH}" != "${BRANCH}" ]]; then
   git checkout "${BASEBRANCH}" || true
-  git branch "${BRANCH}" || { 
+  git branch --set-upstream-to="origin/${BRANCH}" "${BRANCH}" -q || { 
     if [[ ${FORCENEWBRANCH} -eq 0 ]]; then 
       echo "[INFO] Branch ${BRANCH} already exists: nothing to do!"
     else 
       echo "[INFO] Branch ${BRANCH} already exists: deleting and recreating branch"
       git push origin ":${BRANCH}"
       git branch "${BRANCH}"
+      git push origin "${BRANCH}"
     fi
   }
-  git pull origin "${BRANCH}" || true
-  git push origin "${BRANCH}"
 fi
